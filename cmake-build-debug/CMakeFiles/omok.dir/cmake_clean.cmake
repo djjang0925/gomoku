6b7d5d8366bc3150dae274a886a11148278fead5
이항우 <bullie_hu@naver.com>
2024-05-24 14:44:33 +0900

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/omok.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/omok.dir/Board.cpp.obj"
+  "CMakeFiles/omok.dir/Board.cpp.obj.d"
+  "CMakeFiles/omok.dir/main.cpp.obj"
+  "CMakeFiles/omok.dir/main.cpp.obj.d"
+  "CMakeFiles/omok.dir/rule.cpp.obj"
+  "CMakeFiles/omok.dir/rule.cpp.obj.d"
+  "omok.exe"
+  "omok.exe.manifest"
+  "omok.lib"
+  "omok.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/omok.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
